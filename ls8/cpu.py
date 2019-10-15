71fb6badf25f9e05b624c3d10423b3bf5e9a0c76
Kayla <6lackmacaroon@gmail.com>
2019-10-15 16:54:39 -0600

    debugging error can't multiply sequence by non-int of type 'list'

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -9,7 +9,8 @@ class CPU:
         """Construct a new CPU."""
         self.ram = [0] * 256   #random access memory = 256 bits     list of 256 zeros
         self.reg = [0] * 8     #registries = 8   list of 8 zeros
-        self.PC = 0            #internal registers, if we need them
+        self.PC = 0            #program counter
+        self.IR = 0            #internal registers, if we need them
         
     #MAR = memory address register
     #accept the ADDRESS to read and return the value stored there
@@ -23,27 +24,35 @@ class CPU:
         # print("ram write", MDR)
         self.ram[MAR] = MDR
 
-    def load(self):
+    def load(self, file_name):
         """Load a program into memory."""
 
         address = 0
-
-        # For now, we've just hardcoded a program:
-
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        program = []
+        if file_name is None:
+            print("error.")  # for stretch
+            sys.exit(1)
+        try:
+            with open(file_name, 'r') as f:
+                for line in f:
+                    # Process comments:
+                    # Ignore anything after a # symbol
+                    comment_split = line.split("#")
+                    # Convert any numbers from binary strings to integers
+                    num = comment_split[0]
+                    try:
+                        x = int(num, 2)
+                    except ValueError:
+                        continue
+                    # print in binary and decimal
+                    print(f"{x:08b}: {x:d}")
+                    program.append(x)
+        except ValueError:
+            print(f"File not found")
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
-
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
@@ -51,6 +60,8 @@ class CPU:
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         #elif op == "SUB": etc
+        # elif op == "MUL":
+        #     self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
@@ -82,26 +93,37 @@ class CPU:
         while running:
             # self.trace()
             IR = self.ram[self.PC]  #IR = instruction register
-            operand_a = self.ram_read(self.PC+1)
-            operand_b = self.ram_read(self.PC+2)
+            # operand_a = self.ram_read(self.PC+1)
+            # operand_b = self.ram_read(self.PC+2)
             LDI = 0b10000010
             PRN = 0b01000111
             HLT = 0b00000001
+            MUL = 0b10100010
             
             # set the value from the register to an integer
             # print("IR", IR)
             # print("op A", operand_a)
             # print("op B", operand_b)
             if IR == LDI:
-                self.reg[operand_a] = [operand_b]
+                reg_a = self.ram[self.PC + 1]
+                reg_b = self.ram[self.PC + 2]
+                self.reg[reg_a] = [reg_b]
                 self.PC += 3
             #Print to the console the decimal integer value stored in the given register
             elif IR == PRN:
-                print(f"print value: {self.reg[operand_a]}")
+                reg_a = self.ram[self.PC + 1]
+                print(f"print value: {self.reg[reg_a]}")
                 self.PC += 2
                 #halt! and exit
+            elif IR == MUL:  # MUL
+                reg_a = self.ram[self.PC + 1]
+                reg_b = self.ram[self.PC + 2]
+                self.reg[reg_a] *= self.reg[reg_b]
+                # self.alu("MUL", reg_a, reg_b)
+                self.PC += 3
             elif IR == HLT:
                 running = False
+                self.PC += 1
             else:
                 print(f"Unknown Instruction: {IR}")
                 sys.exit(1)

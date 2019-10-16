06ab2cc87ec885eee90a85b6f270aace11b9c6a2
Kayla <6lackmacaroon@gmail.com>
2019-10-15 20:06:50 -0600

    tuesday mvp

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,76 +1,64 @@
 """CPU functionality."""
-
 import sys
-
 class CPU:
     """Main CPU class."""
-
     def __init__(self):
         """Construct a new CPU."""
-        self.ram = [0] * 256   #random access memory = 256 bits     list of 256 zeros
-        self.reg = [0] * 8     #registries = 8   list of 8 zeros
-        self.PC = 0            #program counter
-        self.IR = 0            #internal registers, if we need them
         
-    #MAR = memory address register
-    #accept the ADDRESS to read and return the value stored there
-    def ram_read(self, MAR):
-        # print("ram read", MAR)
-        return self.ram[MAR]
+        self.ram = [0] * 256
+        self.register = [0] * 8
+        self.PC = 0
         
-    #MDR = memory data register
-    #accept the VALUE/DATA to write, and the ADDRESS to write it to
-    def ram_write(self, MDR, MAR):
-        # print("ram write", MDR)
-        self.ram[MAR] = MDR
-
-    def load(self, file_name):
+    def load(self):
         """Load a program into memory."""
-
         address = 0
-        program = []
-        if file_name is None:
-            print("error.")  # for stretch
-            sys.exit(1)
+        # For now, we've just hardcoded a program:
+        # program = []
+        
+        # if sys.argv[1] is None:
+        #     print("This file is bad.")  # for stretch
+        #     sys.exit(1)
         try:
-            with open(file_name, 'r') as f:
+            with open(sys.argv[1]) as f:
                 for line in f:
+                    if line[0] != "#" and line != "\n":
+                        self.ram[address] = int(line[:8], 2)
+                        address += 1
+                f.closed
                     # Process comments:
                     # Ignore anything after a # symbol
-                    comment_split = line.split("#")
-                    # Convert any numbers from binary strings to integers
-                    num = comment_split[0]
-                    try:
-                        x = int(num, 2)
-                    except ValueError:
-                        continue
-                    # print in binary and decimal
-                    print(f"{x:08b}: {x:d}")
-                    program.append(x)
+                    # comment_split = line.split("#")
+                    # # Convert any numbers from binary strings to integers
+                    # num = comment_split[0]
+                    # try:
+                    #     x = int(num, 2)
+                    # except ValueError:
+                    #     continue
+                    # # print in binary and decimal
+                    # print(f"{x:08b}: {x:d}")
+                    # program.append(x)
         except ValueError:
             print(f"File not found")
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
-
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
-
         if op == "ADD":
-            self.reg[reg_a] += self.reg[reg_b]
-        #elif op == "SUB": etc
-        # elif op == "MUL":
-        #     self.reg[reg_a] *= self.reg[reg_b]
+            self.register[reg_a] += self.register[reg_b]
+        elif op == "MUL":
+            self.register[reg_a] *= self.register[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
-
+    def ram_read(self, MAR):
+        return self.ram[MAR]
+    def ram_write(self, MDR, MAR):
+        self.ram[MAR] = MDR
     def trace(self):
         """
         Handy function to print out the CPU state. You might want to call this
         from run() if you need help debugging.
         """
-
         print(f"TRACE: %02X | %02X %02X %02X |" % (
             self.PC,
             #self.fl,
@@ -79,51 +67,38 @@ class CPU:
             self.ram_read(self.PC + 1),
             self.ram_read(self.PC + 2)
         ), end='')
-
         for i in range(8):
-            print(" %02X" % self.reg[i], end='')
-
+            print(" %02X" % self.register[i], end='')
         print()
-
     def run(self):
         """Run the CPU."""
-        #read the memory address that's stored in register `PC`
+        PRN = 0b01000111
+        HLT = 0b00000001
+        LDI = 0b10000010
+        MUL = 0b10100010
         running = True
-        
         while running:
-            # self.trace()
-            IR = self.ram[self.PC]  #IR = instruction register
-            # operand_a = self.ram_read(self.PC+1)
-            # operand_b = self.ram_read(self.PC+2)
-            LDI = 0b10000010
-            PRN = 0b01000111
-            HLT = 0b00000001
-            MUL = 0b10100010
-            
-            # set the value from the register to an integer
-            # print("IR", IR)
-            # print("op A", operand_a)
-            # print("op B", operand_b)
+            IR = self.ram[self.PC]
+            operandA = self.ram_read(self.PC + 1)
+            operandB = self.ram_read(self.PC + 2)
             if IR == LDI:
-                reg_a = self.ram[self.PC + 1]
-                reg_b = self.ram[self.PC + 2]
-                self.reg[reg_a] = [reg_b]
+                # operandA = self.ram_read(self.PC + 1)
+                # operandB = self.ram_read(self.PC + 2)
+                self.register[operandA] = operandB
                 self.PC += 3
-            #Print to the console the decimal integer value stored in the given register
             elif IR == PRN:
-                reg_a = self.ram[self.PC + 1]
-                print(f"print value: {self.reg[reg_a]}")
+                operandA = self.ram_read(self.PC + 1)
+                print(self.register[operandA])
                 self.PC += 2
-                #halt! and exit
-            elif IR == MUL:  # MUL
-                reg_a = self.ram[self.PC + 1]
-                reg_b = self.ram[self.PC + 2]
-                self.reg[reg_a] *= self.reg[reg_b]
-                # self.alu("MUL", reg_a, reg_b)
-                self.PC += 3
             elif IR == HLT:
                 running = False
                 self.PC += 1
+            elif IR == MUL:
+                # operandA = self.ram[self.PC + 1]
+                # operandB = self.ram[self.PC + 2]
+                # self.register[operandA] *= self.register[operandB]
+                self.alu("MUL", operandA, operandB)
+                self.PC += 3
             else:
-                print(f"Unknown Instruction: {IR}")
+                print(f"Unknown Instruction {IR}")
                 sys.exit(1)
